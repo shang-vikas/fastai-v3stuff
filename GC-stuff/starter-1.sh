6b0dddbe321319706a7fbb19f4567bf2f1562461
Vikas <vikas.sangwan@untroddenlabs.com>
2018-10-27 04:19:29 +0530

    structed files

--- GC-stuff/starter-1.sh
+++ GC-stuff/starter-1.sh
@@ -2,4 +2,6 @@
 
 mkdir -p /ext
 mount /dev/sdb /ext
-
+chmod a+w -R /ext
+/opt/anaconda3/bin/conda update conda --yes
+/opt/ananconda3/bin/conda update -c fastai fastai --yes
--- GC-stuff/starter-2.sh
+++ GC-stuff/starter-2.sh
@@ -3,4 +3,6 @@
 mkdir -p /ext
 mkfs.ext4 -m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb
 mount /dev/sdb /ext
-
+chmod a+w -R /ext
+/opt/anaconda3/bin/conda update conda --yes
+/opt/ananconda3/bin/conda update -c fastai fastai --yes
